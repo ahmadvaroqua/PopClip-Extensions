efdbe8fb9cac5efb629632263e4f990fc4a18a2d
Ahmad Varoqua <ahmad.varoqua@gmail.com>
2014-04-05 17:53:20 -0700

    VG > IT > Creating PopClip extension for TimeStream

---
+++ TimeStream.popclipext/TimeStream-Auth.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'json'
+require 'net/http'
+require 'logger'
+require 'uri'
+
+logger = Logger.new('/Users/ahmadvaroqua/Downloads/development.log')
+
+# Get the username and password
+username = "ahmadvaroqua"
+password = "auasole"
+#username = ENV['POPCLIP_OPTION_USERNAME']
+#password = ENV['POPCLIP_OPTION_PASSWORD']
+
+# Hit the login API and capture the response
+uri = URI.parse("https://timestreamapp.com/login.txt")
+response = Net::HTTP.post_form(uri, {:username => "#{username}", :password => "#{password}"})
+
+# Check for authentication
+if response.body == 'Success: Valid credentials'
+  logger.info "#{response}"
+  exit 0
+else
+  logger.error "#{response}"
+  exit 1
+end
--- TimeStream.popclipext/TimeStream.sh
+++ TimeStream.popclipext/TimeStream.sh
@@ -1,1 +1,3 @@
-ts new "$POPCLIP_TEXT"
+# echo "$POPCLIP_OPTION_USERNAME" | say
+# ts new "$POPCLIP_TEXT"
+curl -d "password=$POPCLIP_OPTION_PASSWORD" -d "task=$POPCLIP_TEXT" -d "source=bash" https://timestreamapp.com/$POPCLIP_OPTION_USERNAME.txt
